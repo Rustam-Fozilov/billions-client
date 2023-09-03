6645b7e0689470fc910220cf52df2c4417e629ed
rustamdotdev <thisisrustam@gmail.com>
2023-09-03 23:12:10 +0300

    change locale successfullt worked 2

--- pages/index.vue
+++ pages/index.vue
@@ -17,16 +17,59 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import {fetchUrl} from '~/composable/fetchUrl';
+import { useRouter, useRoute } from 'vue-router';
 
+const route = useRoute()
 const router = useRouter()
-const { locale, setLocale } = useI18n()
+const config = useRuntimeConfig()
+const { setLocale } = useI18n()
+const { data, load } = fetchUrl()
+
+await load(`${config.public.apiUrl}/guest-settings`)
+setLocale(data.value.data[0].value.details.code)
 
 
-if (locale.value === 'ru') {
-    router.push('/ru')
-} else if (locale.value === 'uz') {
-    router.push('/uz')
+if (route.path === '/') {
+    
+    const localeCode = data.value.data[0].value.details.code
+    // setLocale(localeCode)
+
+
+    if (localeCode === 'uz') {
+        router.push('/uz')
+    } else if (localeCode === 'ru') {
+        router.push('/ru')
+    }
+
 }
+
+// else if (route.path === '/ru') {
+
+//     await load(`${config.public.apiUrl}/guest-settings/1`, {
+//         'setting_id': 1,
+//         'value_id': 11,
+//     }, 'PUT')
+        
+
+//     setLocale('ru')
+
+//     router.push(`/ru`)
+
+// } else if (route.path === '/uz') {
+
+//     await load(`${config.public.apiUrl}/guest-settings/1`, {
+//         'setting_id': 1,
+//         'value_id': 10,
+//     }, 'PUT')
+        
+
+//     setLocale('uz')
+
+//     router.push(`/uz`)
+// }
+
+
+
 
 </script>
