4322bcb3301ed1d7236cb29cf35a22590cb2a03e
Rustam-Fozilov <fozilovrustam32@gmail.com>
2023-12-11 15:17:20 +0500

    book cover image changed

--- components/ui/book-card.vue
+++ components/ui/book-card.vue
@@ -2,7 +2,7 @@
     <div id="books-card">
         <div class="w-72 lg:w-full">
             <div @click="gotoBook" class="w-full h-80 sm:h-48 cursor-pointer ">
-                <img class="w-full h-full object-cover" :src="`${config.public.imageUrl}/images/books/book-cover.png`" alt="book cover">
+                <img class="w-full h-full object-cover" :src="`${config.public.imageUrl}/images/books/${book.images[0].link}`" alt="book cover">
             </div>
             <div class="mt-5 flex flex-col gap-2 font-onest-regular">
                 <div class="flex justify-between">
